f54cdb14b2dcd91dbced814670cacf03efa693f4
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2024-05-03 13:51:10 -0300

    style: logo quality on header

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,11 +1,16 @@
 import { MenuIcon } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import { Button } from "./ui/button";
 
 const Header = () => {
   return (
     <div className="flex justify-between px-5 pt-6">
-      <Image src="/fullstack-logo.png" alt="" width={100} height={30} />
+      <div className="relative h-[30px] w-[100px]">
+        <Link href="/">
+          <Image src="/fullstack-logo.png" alt="New Food" fill />
+        </Link>
+      </div>
       <Button
         size="icon"
         variant="outline"
