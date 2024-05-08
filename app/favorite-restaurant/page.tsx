83ecd48a12ae3f9cb80ed69da0a03c5e1dc74198
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2024-05-08 14:05:16 -0300

    feat: add desktop media query for favorite restaurants

--- app/favorite-restaurant/page.tsx
+++ app/favorite-restaurant/page.tsx
@@ -21,7 +21,7 @@ const MyFavoriteRestaurant = async () => {
   return (
     <>
       <Header />
-      <div className="px-5 py-6 md:px-20">
+      <div className="px-5 py-6 md:px-20 lg:px-36 xlg:px-48">
         <h2 className="mb-6 text-lg font-semibold">Restaurantes Favoritos</h2>
         <div className="flex flex-col gap-6 md:grid md:grid-cols-3">
           {userFavoriteRestaurants.length > 0 ? (
